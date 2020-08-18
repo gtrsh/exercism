5321138811f1af3d1d7fb18640aaf64bc9227b8a
Gregory Tereshko <gtrsh@users.noreply.github.com>
2020-08-18 20:06:10 +0300

    [node-express]: split router for each resource

--- node_express/pony-express/index.js
+++ node_express/pony-express/index.js
@@ -6,17 +6,19 @@ const users = require('./fixtures/users');
 const emails = require('./fixtures/emails');
 
 const app = express();
-const router = express.Router();
+const usersRouter = express.Router();
+const emailsRouter = express.Router();
 
 const getUsersRoute = (req, res) => res.send(users);
 const getUserRoute = (req, res) => res.send(users.find(user => user.id === req.params.id));
 const getEmailsRoute = (req, res) => res.send(emails);
 const getEmailRoute = (req, res) => res.send(emails.find(email => email.id === req.params.id));
 
-router.get('/users', getUsersRoute);
-router.get('/users/:id', getUserRoute);
-router.get('/emails', getEmailsRoute);
-router.get('/emails/:id', getEmailRoute);
+usersRouter.get('/', getUsersRoute);
+usersRouter.get('/:id', getUserRoute);
+emailsRouter.get('/', getEmailsRoute);
+emailsRouter.get('/:id', getEmailRoute);
 
-app.use(router);
+app.use('/users', usersRouter);
+app.use('/emails', emailsRouter);
 app.listen(8090);
