8d148abd27bda52abf2856dde6259e98744bcd78
Gregory Tereshko <gtrsh@users.noreply.github.com>
2020-08-18 15:52:32 +0300

    [node-express]: use express Router instead our own

--- node_express/pony-express/index.js
+++ node_express/pony-express/index.js
@@ -1,26 +1,18 @@
+'use strict';
+
 const express = require('express');
 
 const users = require('./fixtures/users');
 const emails = require('./fixtures/emails');
 
 const app = express();
+const router = express.Router();
 
 const getUsersRoute = (req, res) => res.send(users);
 const getEmailsRoute = (req, res) => res.send(emails);
-const noRouteFound = (req, res) => res.end(`You asked for ${req.url}`);
 
-const routes = {
-    'GET /users': getUsersRoute,
-    'GET /emails': getEmailsRoute,
-};
-
-const router = (req, res) => {
-    const route = req.method + ' ' + req.url;
-    const handler = routes[route] || noRouteFound;
-
-    handler(req, res);
-}
+router.get('/users', getUsersRoute);
+router.get('/emails', getEmailsRoute);
 
 app.use(router);
-
 app.listen(8090);
