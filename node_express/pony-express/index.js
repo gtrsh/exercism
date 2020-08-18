5799911d1f92e18aa50d4d42b09735444fbe2a5d
Gregory Tereshko <gtrsh@users.noreply.github.com>
2020-08-18 19:56:40 +0300

    [node-express]: add dynamic segments to routes

--- node_express/pony-express/index.js
+++ node_express/pony-express/index.js
@@ -9,10 +9,14 @@ const app = express();
 const router = express.Router();
 
 const getUsersRoute = (req, res) => res.send(users);
+const getUserRoute = (req, res) => res.send(users.find(user => user.id === req.params.id));
 const getEmailsRoute = (req, res) => res.send(emails);
+const getEmailRoute = (req, res) => res.send(emails.find(email => email.id === req.params.id));
 
 router.get('/users', getUsersRoute);
+router.get('/users/:id', getUserRoute);
 router.get('/emails', getEmailsRoute);
+router.get('/emails/:id', getEmailRoute);
 
 app.use(router);
 app.listen(8090);
