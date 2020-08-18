757a22bd36d6fc34e7f22a19f9375711fa229dee
Gregory Tereshko <gtrsh@users.noreply.github.com>
2020-08-19 02:05:30 +0300

    [node-express]: implement creating new email

--- node_express/pony-express/routes/emails.js
+++ node_express/pony-express/routes/emails.js
@@ -2,13 +2,24 @@
 
 const express = require('express');
 const emails = require('../fixtures/emails');
+const readBody = require('../utils/read-body');
+const generateId = require('../utils/gen-id');
 
 const emailsRouter = express.Router();
 
 const getEmailsRoute = (req, res) => res.send(emails);
 const getEmailRoute = (req, res) => res.send(emails.find(email => email.id === req.params.id));
+const createEmailRoute = async (req, res) => {
+    const body = await readBody(req);
+    const newEmail = {...JSON.parse(body), id: generateId()};
+    emails.push(newEmail);
+
+    res.status(201);
+    res.send(newEmail);
+};
 
 emailsRouter.get('/', getEmailsRoute);
 emailsRouter.get('/:id', getEmailRoute);
+emailsRouter.post('/', createEmailRoute);
 
 module.exports = emailsRouter;
