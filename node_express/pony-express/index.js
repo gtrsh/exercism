3a26d2459f615eec2c0c5e54471dd90482b9c1f4
Gregory Tereshko <gtrsh@users.noreply.github.com>
2020-08-18 15:27:46 +0300

    [node-express]: eliminate if/else statement for routes

--- node_express/pony-express/index.js
+++ node_express/pony-express/index.js
@@ -7,17 +7,18 @@ const app = express();
 
 const getUsersRoute = (req, res) => res.send(users);
 const getEmailsRoute = (req, res) => res.send(emails);
+const noRouteFound = (req, res) => res.end(`You asked for ${req.url}`);
+
+const routes = {
+    'GET /users': getUsersRoute,
+    'GET /emails': getEmailsRoute,
+};
 
 app.use((req, res) => {
     const route = req.method + ' ' + req.url;
+    const handler = routes[route];
 
-    if (route === 'GET /users') {
-        getUsersRoute(req, res);
-    } else if (route === 'GET /emails') {
-        getEmailsRoute(req, res);
-    } else {
-        res.end(`You asked for ${route}`);
-    }
+    handler ? handler(req, res) : noRouteFound(req, res);
 });
 
 app.listen(8090);
