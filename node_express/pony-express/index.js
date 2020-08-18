698d1d0d8b7e0470c176be760409c955e8bfdf3b
Gregory Tereshko <gtrsh@users.noreply.github.com>
2020-08-18 15:02:14 +0300

    [node-express]: extract request handlers into separate functions

--- node_express/pony-express/index.js
+++ node_express/pony-express/index.js
@@ -5,13 +5,16 @@ const emails = require('./fixtures/emails');
 
 const app = express();
 
+const getUsersRoute = (req, res) => res.send(users);
+const getEmailsRoute = (req, res) => res.send(emails);
+
 app.use((req, res) => {
     const route = req.method + ' ' + req.url;
 
     if (route === 'GET /users') {
-        res.send(users);
+        getUsersRoute(req, res);
     } else if (route === 'GET /emails') {
-        res.send(emails);
+        getEmailsRoute(req, res);
     } else {
         res.end(`You asked for ${route}`);
     }
