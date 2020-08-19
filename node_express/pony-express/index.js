3bdcc6267e9ca031a66872de0a68af2c6e5d9994
Gregory Tereshko <gtrsh@users.noreply.github.com>
2020-08-19 23:40:15 +0300

    [node-express]: add example middleware

--- node_express/pony-express/index.js
+++ node_express/pony-express/index.js
@@ -4,9 +4,11 @@ const express = require('express');
 
 const usersRouter = require('./routes/users');
 const emailsRouter = require('./routes/emails');
+const logger = require('./middlewares/logger');
 
 const app = express();
 
+app.use(logger);
 app.use('/users', usersRouter);
 app.use('/emails', emailsRouter);
 app.listen(8090);
