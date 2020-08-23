e1596f835f810c2a05b2f4672e5b9135485cf74d
Gregory Tereshko <gtrsh@users.noreply.github.com>
2020-08-23 17:02:00 +0300

    [node-express]: add middleware for json-body parsing

---
+++ node_express/pony-express/middlewares/json_body_parser.js
@@ -0,0 +1,10 @@
+const readBody = require('../utils/read-body');
+
+const jsonBodyParser = async (req, res, next) => {
+    const body = await readBody(req);
+    req.body = JSON.parse(body);
+
+    next();
+};
+
+module.exports = jsonBodyParser;
