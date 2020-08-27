1d0ea9d35ff8f3fb4abb67653b20adde0e74683c
Gregory Tereshko <gtrsh@users.noreply.github.com>
2020-08-27 20:08:04 +0300

    [node-express]: get user from db and add to req object

--- node_express/pony-express/middlewares/basic-auth.js
+++ node_express/pony-express/middlewares/basic-auth.js
@@ -1,9 +1,28 @@
+const users = require('../fixtures/users');
+
+const findUserByCredentials = ({ username, password }) =>
+    users.find(user => user.username === username && user.password === password)
+
 const basicAuth = (req, res, next) => {
     const header = req.headers.authorization || '';
     const [type, payload] = header.split(' ');
 
     console.log(`AUTH LOG type: ${type}`);
     console.log(`AUTH LOG payload: ${payload}`);
+
+    if (type === 'Basic') {
+        const credentials = Buffer.from(payload, 'base64').toString('ascii')
+        const [username, password] = credentials.split(':');
+
+        const user = findUserByCredentials({ username, password })
+
+        if (user) {
+            req.user = user;
+        } else {
+            res.sendStatus(401);
+            return ;
+        }
+    }
 
     next();
 }
