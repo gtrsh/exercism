2f3c4d000f0748efcabd9b072b62ed7a70f42578
Gregory Tereshko <gtrsh@users.noreply.github.com>
2021-07-31 03:37:24 +0300

    [hexlet]: *objectss* 12 fn for destructure object

---
+++ hexlet-node/objects/destructureObject.js
@@ -0,0 +1,12 @@
+/*
+const users = [
+  { name: 'Bronn', gender: 'male', birthday: '1973-03-23' },
+  { name: 'Reigar', gender: 'male', birthday: '1973-11-03' },
+  { name: 'Eiegon', gender: 'male', birthday: '1963-11-03' },
+  { name: 'Sansa', gender: 'female', birthday: '2012-11-03' },
+];
+
+getSortedNames(users); // ['Bronn', 'Eiegon', 'Reigar', 'Sansa']
+ */
+
+export default (xs) => xs.reduce((acc, { name }) => [...acc, name].sort(), []);
