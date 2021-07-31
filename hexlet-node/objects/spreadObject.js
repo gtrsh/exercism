f1d21f0c082e2409641a28120014a7e2efc8a8d0
Gregory Tereshko <gtrsh@users.noreply.github.com>
2021-07-31 03:35:51 +0300

    [hexlet]: *objectss* 11 fn for return object and use spread

---
+++ hexlet-node/objects/spreadObject.js
@@ -0,0 +1,23 @@
+/*
+const company = make('Hexlet');
+// {
+//   name: 'Hexlet',
+//   state: 'moderating',
+//   createdAt: <тут текущая дата>
+// }
+
+const company = make('Hexlet', { website: 'hexlet.io', state: 'published' });
+// {
+//   name: 'Hexlet',
+//   website: 'hexlet.io',
+//   state: 'published',
+//   createdAt: <тут текущая дата>
+// }
+ */
+
+export default (name, options = {}) => ({
+    name,
+    state: 'moderating',
+    createdAt: Date.now(),
+    ...options,
+});
