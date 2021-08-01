006fe5e3953aeda35ddd286ce311e5cb2eadd68b
Gregory Tereshko <gtrsh@users.noreply.github.com>
2021-08-01 09:53:12 +0300

    [hexlet]: *fn* 09 high order map fn example

---
+++ hexlet-node/fn/fnMap.js
@@ -0,0 +1,37 @@
+/*
+const users = [
+  {
+    name: 'Tirion',
+    children: [
+      { name: 'Mira', birthday: '1983-03-23' },
+    ],
+  },
+  { name: 'Bronn', children: [] },
+  {
+    name: 'Sam',
+    children: [
+      { name: 'Aria', birthday: '2012-11-03' },
+      { name: 'Keit', birthday: '1933-05-14' },
+    ],
+  },
+  {
+    name: 'Rob',
+    children: [
+      { name: 'Tisha', birthday: '2012-11-03' },
+    ],
+  },
+];
+
+getChildren(users);
+// [
+//   { name: 'Mira', birthday: '1983-03-23' },
+//   { name: 'Aria', birthday: '2012-11-03' },
+//   { name: 'Keit', birthday: '1933-05-14' },
+//   { name: 'Tisha', birthday: '2012-11-03' },
+// ];
+ */
+
+// Alternative solution
+// export default (users) => users.flatMap(({ children }) => children);
+
+export default (xs) => xs.map(({ children }) => children).flat();
