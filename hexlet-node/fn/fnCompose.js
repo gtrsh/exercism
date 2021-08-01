5234d802c24effd2916602b462b007f8ce2a9b8a
Gregory Tereshko <gtrsh@users.noreply.github.com>
2021-08-02 00:17:07 +0300

    [hexlet]: *fn* 12 map/filter/reduce composition fn example

---
+++ hexlet-node/fn/fnCompose.js
@@ -0,0 +1,47 @@
+/*
+const emails = [
+    'info@gmail.com',
+    'info@yandex.ru',
+    'info@hotmail.com',
+    'mk@host.com',
+    'support@hexlet.io',
+    'key@yandex.ru',
+    'sergey@gmail.com',
+    'vovan@gmail.com',
+    'vovan@hotmail.com',
+];
+
+getFreeDomainsCount(emails);
+// {
+//   'gmail.com': 3,
+//   'yandex.ru': 2,
+//   'hotmail.com': 2,
+// };
+ */
+
+const freeEmailDomains = [
+    'gmail.com',
+    'yandex.ru',
+    'hotmail.com',
+];
+
+const getDomains = (email) => {
+    const tmp = email.split('@');
+    return tmp[tmp.length - 1];
+};
+
+const filterFreeDomainEmails = (domain) => freeEmailDomains.includes(domain);
+
+const reduceDomains = (acc, domain) => {
+    acc[domain] = (acc[domain] ?? 0) + 1;
+    return acc;
+};
+
+const getFreeDomainsCount = (emails) => (
+    emails
+        .map(getDomains)
+        .filter(filterFreeDomainEmails)
+        .reduce(reduceDomains, {})
+);
+
+export default getFreeDomainsCount;
