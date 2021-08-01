398c8489f52a1db67dad693d51742898bf8b245d
Gregory Tereshko <gtrsh@users.noreply.github.com>
2021-08-01 10:09:18 +0300

    [hexlet]: *fn* 10 high order filter fn example

---
+++ hexlet-node/fn/fnFilter.js
@@ -0,0 +1,34 @@
+/*
+const users = [
+  {
+    name: 'Tirion',
+    friends: [
+      { name: 'Mira', gender: 'female' },
+      { name: 'Ramsey', gender: 'male' },
+    ],
+  },
+  { name: 'Bronn', friends: [] },
+  {
+    name: 'Sam',
+    friends: [
+      { name: 'Aria', gender: 'female' },
+      { name: 'Keit', gender: 'female' },
+    ],
+  },
+  {
+    name: 'Rob',
+    friends: [
+      { name: 'Taywin', gender: 'male' },
+    ],
+  },
+];
+
+getGirlFriends(users);
+// [
+//   { name: 'Mira', gender: 'female' },
+//   { name: 'Aria', gender: 'female' },
+//   { name: 'Keit', gender: 'female' },
+// ];
+ */
+
+export default (xs) => xs.flatMap(({ friends }) => friends).filter(({ gender }) => gender === 'female');
