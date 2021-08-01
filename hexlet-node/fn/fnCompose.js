af7124ee37d34fe2290d211769b820be7d6778a2
Gregory Tereshko <gtrsh@users.noreply.github.com>
2021-08-02 00:20:35 +0300

    [hexlet]: *fn* 12 add lodash version

--- hexlet-node/fn/fnCompose.js
+++ hexlet-node/fn/fnCompose.js
@@ -19,6 +19,24 @@ getFreeDomainsCount(emails);
 // };
  */
 
+/*
+LODASH VERSION:
+import get from 'lodash/get';
+
+const getFreeDomainsCount = (emails) => emails
+  .map((email) => {
+    const [, domain] = email.split('@');
+    return domain;
+  })
+  .filter((domain) => freeEmailDomains.includes(domain))
+  .reduce((acc, domain) => {
+    const count = get(acc, domain, 0) + 1;
+    return { ...acc, [domain]: count };
+  }, {});
+
+export default getFreeDomainsCount;
+ */
+
 const freeEmailDomains = [
     'gmail.com',
     'yandex.ru',
