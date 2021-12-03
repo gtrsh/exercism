9d80838f3c2d734bb49ccc321a29d0acf69f550b
Gregory Tereshko <gtrsh@users.noreply.github.com>
2021-12-04 00:24:51 +0300

    [lisp-racket-htdp]: image lib examples

--- racket-htdp/00_prologue/prologue.rkt
+++ racket-htdp/00_prologue/prologue.rkt
@@ -1,5 +1,7 @@
 #lang htdp/bsl
 
+(require 2htdp/image)
+
 (+ 1 4)
 (* 3 3)
 (- 4 2)
@@ -22,9 +24,28 @@
 (string-length
  (string-append "make" " " "the cake"))
 (string->number "42")
+(string? "aaa")
+(string? 42)
+(string=? "racket" "racket-lang")
 
 (and #t #t)
 (and #t #f)
 (or #t #t)
 (or #t #f)
 (not #t)
+
+(> 10 9)
+(< -1 0)
+(= 42 (* 21 2))
+
+(circle 60 "solid" "pink")
+(above (triangle 40 "solid" "red")
+       (rectangle 40 30 "solid" "blue"))
+(overlay (circle 15 "solid" "red")
+         (rectangle 60 60 "solid" "blue"))
+(image-width (square 10 "solid" "red"))
+
+(empty-scene 100 100)
+(place-image (circle 5 "solid" "green")
+             40 80
+             (empty-scene 200 200))
