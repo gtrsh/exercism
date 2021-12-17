eb034ddd8d269f4b7b0420b16e668504d45d6009
Gregory Tereshko <gtrsh@users.noreply.github.com>
2021-12-17 03:34:51 +0300

    [lisp-racket-htdp]: on key event example

--- racket-htdp/01_fixed-size-data/fns-io-universe.rkt
+++ racket-htdp/01_fixed-size-data/fns-io-universe.rkt
@@ -5,3 +5,13 @@
 
 (define (number->square s)
   (square s "solid" "blue"))
+
+(define (reset s ke)
+  100)
+
+(big-bang
+ 100
+ [on-key reset]
+ [on-tick sub1]
+ [stop-when zero?]
+ [to-draw number->square])
