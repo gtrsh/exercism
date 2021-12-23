707bdec21bed57f57a6d1630e037b811b5386ee8
Gregory Tereshko <gtrsh@users.noreply.github.com>
2021-12-23 16:21:51 +0300

    [hexlet]: data-abs: split tests into test-suites

--- hexlet-node/data-abstraction-tasks/__tests__/points.test.js
+++ hexlet-node/data-abstraction-tasks/__tests__/points.test.js
@@ -3,36 +3,40 @@ import {
   getMidpoint
 } from '../src/points.js'
 
-test('point1', () => {
-  const point1 = [0, 0]
-  const point2 = [3, 4]
-  expect(calculateDistance(point1, point2)).toBe(5)
+describe('Calculate Distance', () => {
+  test('point1', () => {
+    const point1 = [0, 0]
+    const point2 = [3, 4]
+    expect(calculateDistance(point1, point2)).toBe(5)
+  })
+
+  test('point2', () => {
+    const point1 = [-1, -4]
+    const point2 = [-1, -10]
+    expect(calculateDistance(point1, point2)).toBe(6)
+  })
+
+  test('point3', () => {
+    const point1 = [1, 10]
+    const point2 = [1, 3]
+    expect(calculateDistance(point1, point2)).toBe(7)
+  })
 })
 
-test('point2', () => {
-  const point1 = [-1, -4]
-  const point2 = [-1, -10]
-  expect(calculateDistance(point1, point2)).toBe(6)
-})
+describe('Calculate Middle point', () => {
+  test('mid point1', () => {
+    const point1 = { x: 0, y: 0 }
+    const point2 = { x: 4, y: 4 }
 
-test('point3', () => {
-  const point1 = [1, 10]
-  const point2 = [1, 3]
-  expect(calculateDistance(point1, point2)).toBe(7)
-})
+    const point = getMidpoint(point1, point2)
+    expect(point).toEqual({ x: 2, y: 2 })
+  })
 
-test('mid point1', () => {
-  const point1 = { x: 0, y: 0 }
-  const point2 = { x: 4, y: 4 }
+  test('mid point2', () => {
+    const point1 = { x: 2, y: 2 }
+    const point2 = { x: 4, y: 4 }
 
-  const point = getMidpoint(point1, point2)
-  expect(point).toEqual({ x: 2, y: 2 })
-})
-
-test('mid point2', () => {
-  const point1 = { x: 2, y: 2 }
-  const point2 = { x: 4, y: 4 }
-
-  const point = getMidpoint(point1, point2)
-  expect(point).toEqual({ x: 3, y: 3 })
+    const point = getMidpoint(point1, point2)
+    expect(point).toEqual({ x: 3, y: 3 })
+  })
 })
