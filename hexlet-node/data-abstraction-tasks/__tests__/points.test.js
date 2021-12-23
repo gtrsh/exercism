d35c924f50ae7fabcd056c0736be5c1af293e15c
Gregory Tereshko <gtrsh@users.noreply.github.com>
2021-12-23 16:18:12 +0300

    [hexlet]: data-abs: done middle point task

--- hexlet-node/data-abstraction-tasks/__tests__/points.test.js
+++ hexlet-node/data-abstraction-tasks/__tests__/points.test.js
@@ -1,4 +1,7 @@
-import { calculateDistance } from '../src/points.js'
+import {
+  calculateDistance,
+  getMidpoint
+} from '../src/points.js'
 
 test('point1', () => {
   const point1 = [0, 0]
@@ -16,4 +19,20 @@ test('point3', () => {
   const point1 = [1, 10]
   const point2 = [1, 3]
   expect(calculateDistance(point1, point2)).toBe(7)
+})
+
+test('mid point1', () => {
+  const point1 = { x: 0, y: 0 }
+  const point2 = { x: 4, y: 4 }
+
+  const point = getMidpoint(point1, point2)
+  expect(point).toEqual({ x: 2, y: 2 })
+})
+
+test('mid point2', () => {
+  const point1 = { x: 2, y: 2 }
+  const point2 = { x: 4, y: 4 }
+
+  const point = getMidpoint(point1, point2)
+  expect(point).toEqual({ x: 3, y: 3 })
 })
--- hexlet-node/data-abstraction-tasks/src/points.js
+++ hexlet-node/data-abstraction-tasks/src/points.js
@@ -1,3 +1,10 @@
 const calculateDistance = ([x1, y1], [x2, y2]) => Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+const getMidpoint = (p1, p2) => ({
+  x: (p1.x + p2.x) / 2,
+  y: (p1.y + p2.y) / 2
+})
 
-export { calculateDistance }
+export {
+  calculateDistance,
+  getMidpoint
+}
