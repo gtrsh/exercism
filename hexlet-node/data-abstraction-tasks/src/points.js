7d11db02a0f235f3e319863181919c683ede5165
Gregory Tereshko <gtrsh@users.noreply.github.com>
2021-12-24 22:19:03 +0300

    [hexlet]: data-abs: add functions for working with points

--- hexlet-node/data-abstraction-tasks/src/points.js
+++ hexlet-node/data-abstraction-tasks/src/points.js
@@ -4,6 +4,14 @@ const getMidpoint = (p1, p2) => ({
   y: (p1.y + p2.y) / 2
 })
 
+const makeDecartPoint = (x, y) => {
+  const point = { x, y };
+  return point;
+};
+
+const getX = (point) => point.x;
+const getY = (point) => point.y;
+
 export {
   calculateDistance,
   getMidpoint
