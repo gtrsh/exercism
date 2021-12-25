6e862ad8eebb84ca688078805ab6dda169386a21
Gregory Tereshko <gtrsh@users.noreply.github.com>
2021-12-25 19:05:11 +0300

    [hexlet]: data-abs: refactor midpoint fn

--- hexlet-node/data-abstraction-tasks/src/points.js
+++ hexlet-node/data-abstraction-tasks/src/points.js
@@ -1,7 +1,7 @@
 const calculateDistance = ([x1, y1], [x2, y2]) => Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 const getMidpoint = (p1, p2) => ({
-  x: (p1.x + p2.x) / 2,
-  y: (p1.y + p2.y) / 2
+  x: (getX(p1) + getX(p2)) / 2,
+  y: (getY(p1) + getY(p2)) / 2
 })
 
 const makeDecartPoint = (x, y) => ({ x, y })
