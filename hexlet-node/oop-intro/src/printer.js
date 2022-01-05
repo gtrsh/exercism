ec99af00068539fffce083c9644c28a12250660f
Gregory Tereshko <gtrsh@users.noreply.github.com>
2022-01-05 10:14:12 +0300

    [hexlet]: oop-intro: example of loosing context

---
+++ hexlet-node/oop-intro/src/printer.js
@@ -0,0 +1,14 @@
+const printer = {
+  name: 'Hexlet',
+  print(greeting = 'hello') {
+    console.log(`${greeting}, ${this.name}`);
+  }
+};
+
+// Прямой запуск
+printer.print();
+
+// Хотим запустить метод print через секунду
+// Обязательно запустите этот код на своем компьютере
+// чтобы почувствовать то как работает setTimeout
+setTimeout(printer.print, 1000);
