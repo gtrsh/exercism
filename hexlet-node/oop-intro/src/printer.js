f68eb09bf1943fabcaf35489c4aefe80a2195ccd
Gregory Tereshko <gtrsh@users.noreply.github.com>
2022-01-05 10:32:23 +0300

    [hexlet]: oop-intro: linter fix

--- hexlet-node/oop-intro/src/printer.js
+++ hexlet-node/oop-intro/src/printer.js
@@ -1,26 +1,26 @@
 const printer = {
   name: 'Hexlet',
-  print(greeting = 'hello') {
-    console.log(`${greeting}, ${this.name}`);
+  print (greeting = 'hello') {
+    console.log(`${greeting}, ${this.name}`)
   }
-};
+}
 
 // Прямой запуск
-printer.print();
+printer.print()
 
 // Хотим запустить метод print через секунду
 // Обязательно запустите этот код на своем компьютере
 // чтобы почувствовать то как работает setTimeout
-setTimeout(printer.print, 1000);
+setTimeout(printer.print, 1000)
 
-setTimeout(() => printer.print(), 1000);
+setTimeout(() => printer.print(), 1000)
 
-const boundPrint = printer.print.bind(printer, 'hey!');
-setTimeout(boundPrint, 1000);
+const boundPrint = printer.print.bind(printer, 'hey!')
+setTimeout(boundPrint, 1000)
 
 // func.apply(thisArg, [ argsArray])
-const print = printer.print;
-print.apply(printer, ['how-how']); // how-how, Hexlet
+const print = printer.print
+print.apply(printer, ['how-how']) // how-how, Hexlet
 
 // func.call([thisArg[, arg1, arg2, ...argN]])
-print.call(printer, 'how-how'); // how-how, Hexlet
+print.call(printer, 'how-how') // how-how, Hexlet
