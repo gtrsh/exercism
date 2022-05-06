ddea052f1efafc8105548736feb80e3f5b9d3729
Gregory Tereshko <gtrsh@users.noreply.github.com>
2022-05-06 18:32:56 +0300

    [hexlet]: async: adjust cb call style

--- hexlet-node/async/src/dirSize.js
+++ hexlet-node/async/src/dirSize.js
@@ -6,15 +6,13 @@ import async from 'async'
 const getDirectorySize = (dir, cb) => {
   fs.readdir(dir, (err, fileNames) => {
     if (err) {
-      cb(err)
-      return
+      return cb(err)
     }
 
     const files = fileNames.map((name) => path.join(dir, name))
     async.map(files, fs.stat, (err, stats) => {
       if (err) {
-        cb(err)
-        return
+        return cb(err)
       }
 
       const filesSizeArray = _.sumBy(stats.filter((stat) => stat.isFile()), 'size')
