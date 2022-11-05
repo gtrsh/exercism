a1c9b56071c3d6c0c9748e5804d6f5572bfaa76e
Gregory Tereshko <gtrsh@users.noreply.github.com>
2022-11-05 09:00:26 +0300

    [sicp]: adjust exercise 1.6

--- node-sicp/src/exercises/exercise-1-6.js
+++ node-sicp/src/exercises/exercise-1-6.js
@@ -20,5 +20,12 @@ const sqrt_iter = (guess, x) => cond(
 const sqrt = (x) => sqrt_iter(1, x)
 
 // RangeError: Maximum call stack size exceeded
-// will happened when we try to call function
+// will happened when we try to call function sqrt
+// because when interpreter call cond fn its try to
+// compute all its arguments which contain sqrt_iter
+// so we occurred in infinite recursion sqrt_iter calls
+//
+// That's why we use ternary operator instead
+// because its lazy and not eval its right part if
+// predicate is true
 console.log(sqrt(9))
