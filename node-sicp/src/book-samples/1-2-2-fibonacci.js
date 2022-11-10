f4f05ba205ef9baad30fc55b4e62eadff9deb888
Gregory Tereshko <gtrsh@users.noreply.github.com>
2022-11-10 08:26:40 +0300

    [sicp]: add recursion fibonacci sample

---
+++ node-sicp/src/book-samples/1-2-2-fibonacci.js
@@ -0,0 +1,21 @@
+/*
+tracing body:
+console.log(`current arg is: ${n}`)
+return (
+  n === 0
+    ? 0
+    : n === 1
+      ? 1
+      : fib(n - 1) + fib(n - 2)
+)
+ */
+
+const fib = (n) => (
+  n === 0
+    ? 0
+    : n === 1
+      ? 1
+      : fib(n - 1) + fib(n - 2)
+)
+
+console.log(fib(8))
