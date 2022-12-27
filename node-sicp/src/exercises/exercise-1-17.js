0c55fc6c2047307bb3d76f4174c9bc1dbf80f36b
Gregory Tereshko <gtrsh@users.noreply.github.com>
2022-12-27 13:14:59 +0300

    [sicp]: use dec function

--- node-sicp/src/exercises/exercise-1-17.js
+++ node-sicp/src/exercises/exercise-1-17.js
@@ -1,14 +1,15 @@
 const is_even = (n) => n % 2 === 0
 const double = (n) => n + n
 const halve = (n) => n / 2
+const dec = (n) => n - 1
 
 const times = (a, n) => {
   return n === 0 ? 0 :
     is_even(n) ? double(times(a, halve(n))) :
-      a + times(a, n - 1)
+      a + times(a, dec(n))
 }
 
 console.log(times(3, 9)) // 27
 
 // console.log(times(8, 1000)) | 8000
-// console.log(fn_call)          | 16
+// console.log(fn_call)        | 16
