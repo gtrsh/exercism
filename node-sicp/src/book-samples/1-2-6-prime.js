69a64964307c8e6f3fec045501f9c5212ff99193
Gregory Tereshko <gtrsh@users.noreply.github.com>
2023-01-02 23:33:49 +0300

    [sicp]: add prime nums fn example

---
+++ node-sicp/src/book-samples/1-2-6-prime.js
@@ -0,0 +1,14 @@
+const square = (n) => n ** 2
+const divides = (a, b) => b % a === 0
+
+const find_divisor = (n, test_divisor) => {
+  return square(test_divisor) > n ? n :
+    divides(test_divisor, n) ? test_divisor :
+      find_divisor(n, test_divisor + 1)
+}
+
+const smallest_divisor = (n) => find_divisor(n, 2)
+const is_prime = (n) => n === smallest_divisor(n)
+
+console.log(is_prime(4)) // false
+console.log(is_prime(13)) // true
