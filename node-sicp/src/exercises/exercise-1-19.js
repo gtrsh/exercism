9fa6f019de59dc3e96f954ea129b763da9873198
Gregory Tereshko <gtrsh@users.noreply.github.com>
2023-01-02 21:14:45 +0300

    [sicp]: complete exercise 1.19

--- node-sicp/src/exercises/exercise-1-19.js
+++ node-sicp/src/exercises/exercise-1-19.js
@@ -1,4 +1,3 @@
-// TODO exercise
 const is_even = (n) => n % 2 === 0
 const halve = (n) => n / 2
 const dec = (n) => n - 1
@@ -10,8 +9,8 @@ const fib_iter = (a, b, p, q, count) => {
         fib_iter(
           a,
           b,
-          // compute p
-          // compute q
+          p ** 2 + q ** 2,
+          2 * p * q + q ** 2,
           halve(count)
         ) :
         fib_iter(
@@ -24,3 +23,6 @@ const fib_iter = (a, b, p, q, count) => {
 }
 
 const fib = (n) => fib_iter(1, 0, 0, 1, n)
+
+console.log(fib(8)) // 21
+console.log(fib(78)) // 8944394323791464 fn_call: 11
