2a1b784d0b30f08aa5442c4621e21ee6bce6093b
Gregory Tereshko <gtrsh@users.noreply.github.com>
2023-02-26 07:36:35 +0300

    [cljs]: use new components in giggin ui

--- cljs-reagent-mooc/src/giggin/core.cljs
+++ cljs-reagent-mooc/src/giggin/core.cljs
@@ -1,9 +1,17 @@
 (ns giggin.core
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [giggin.components.header :refer [header]]
+            [giggin.components.footer :refer [footer]]
+            [giggin.components.orders :refer [orders]]
+            [giggin.components.gigs :refer [gigs]]))
 
 (defn app
   []
-  [:div.container])
+  [:div.container
+   [header]
+   [gigs]
+   [orders]
+   [footer]])
 
 (defn ^:export main
   []
