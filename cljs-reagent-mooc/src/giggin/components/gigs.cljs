72a2cbbda719607d74f73634aff85a6fd95d5a73
Gregory Tereshko <gtrsh@users.noreply.github.com>
2023-02-27 09:47:47 +0300

    [cljs] add for loop example for rendering gigs

--- cljs-reagent-mooc/src/giggin/components/gigs.cljs
+++ cljs-reagent-mooc/src/giggin/components/gigs.cljs
@@ -13,7 +13,19 @@
            [:p.gig__price (:price gig)]
            [:p.gig__desc (:desc gig)]]]) (vals @state/gigs)))
 
+(defn get-gigs-compr
+  []
+  (for [{:keys [id img title price desc]} (vals @state/gigs)]
+    [:div.gig {:key id}
+     [:img.gig__artwork {:src img :alt title}]
+     [:div.gig__body
+      [:div.gig__title
+       [:div.btn.btn--primary.float--right.tooltip {:data-tooltip "Add to order"}
+        [:i.icon.icon--plus]] title]
+      [:p.gig__price price]
+      [:p.gig__desc desc]]]))
+
 (defn gigs
   []
   [:main
-   [:div.gigs (get-gigs)]])
+   [:div.gigs (get-gigs-compr)]])
