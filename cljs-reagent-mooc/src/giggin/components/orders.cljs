8427dcbdbb0240490501e98fd24849898d954631
Gregory Tereshko <gtrsh@users.noreply.github.com>
2023-03-02 19:51:15 +0300

    [cljs]: show suggestion when orders state is empty

--- cljs-reagent-mooc/src/giggin/components/orders.cljs
+++ cljs-reagent-mooc/src/giggin/components/orders.cljs
@@ -10,28 +10,32 @@
 (defn orders
   []
   [:aside
-   [:div.order
-    [:div.body
-     (for [[id quant] @state/orders]
-       [:div.item {:key id}
-        [:div.img
-         [:img {:src (get-in @state/gigs [id :img])
-                :alt (get-in @state/gigs [id :title])}]]
-        [:div.content
-         [:p.title (str (get-in @state/gigs [id :title]) " \u00D7 " quant)]]
+   (if (empty? @state/orders)
+     [:div.empty
+      [:div.title "Tou don't have any orders"]
+      [:div.subtitle "Click on a + to add an order"]]
+     [:div.order
+      [:div.body
+       (for [[id quant] @state/orders]
+         [:div.item {:key id}
+          [:div.img
+           [:img {:src (get-in @state/gigs [id :img])
+                  :alt (get-in @state/gigs [id :title])}]]
+          [:div.content
+           [:p.title (str (get-in @state/gigs [id :title]) " \u00D7 " quant)]]
+          [:div.action
+           [:div.price (* (get-in @state/gigs [id :price]) quant)]
+           [:button.btn.btn--link.tooltip
+            {:data-tooltip "Remove"
+             :on-click #(swap! state/orders dissoc id)}
+            [:i.icon.icon--cross]]]])]
+      [:div.total
+       [:hr]
+       [:div.item
+        [:div.content "Total"]
         [:div.action
-         [:div.price (* (get-in @state/gigs [id :price]) quant)]
-         [:button.btn.btn--link.tooltip
-          {:data-tooltip "Remove"
-           :on-click #(swap! state/orders dissoc id)}
-          [:i.icon.icon--cross]]]])]
-    [:div.total
-     [:hr]
-     [:div.item
-      [:div.content "Total"]
-      [:div.action
-       [:div.price (total)]]
-      [:button.btn.btn--link.tooltip
-       {:data-tooltip "Remove all"
-        :on-click #(reset! state/orders {})}
-       [:i.icon.icon--delete]]]]]])
+         [:div.price (total)]]
+        [:button.btn.btn--link.tooltip
+         {:data-tooltip "Remove all"
+          :on-click #(reset! state/orders {})}
+         [:i.icon.icon--delete]]]]])])
