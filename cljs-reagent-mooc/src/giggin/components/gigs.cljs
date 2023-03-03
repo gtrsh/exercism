2ef597a8fa1edc8792fc252d85722577941e5c50
Gregory Tereshko <gtrsh@users.noreply.github.com>
2023-03-03 20:01:26 +0300

    [cljs]: remove fn with map example

--- cljs-reagent-mooc/src/giggin/components/gigs.cljs
+++ cljs-reagent-mooc/src/giggin/components/gigs.cljs
@@ -4,18 +4,6 @@
 
 (defn get-gigs
   []
-  (map (fn [gig]
-         [:div.gig {:key (:id gig)}
-          [:img.gig__artwork {:src (:img gig) :alt (:title gig)}]
-          [:div.gig__body
-           [:div.gig__title
-            [:div.btn.btn--primary.float--right.tooltip {:data-tooltip "Add to order"}
-             [:i.icon.icon--plus]] (:title gig)]
-           [:p.gig__price (:price gig)]
-           [:p.gig__desc (:desc gig)]]]) (vals @state/gigs)))
-
-(defn get-gigs-compr
-  []
   (for [{:keys [id img title price desc]} (vals @state/gigs)]
     [:div.gig {:key id}
      [:img.gig__artwork {:src img :alt title}]
@@ -31,4 +19,4 @@
 (defn gigs
   []
   [:main
-   [:div.gigs (get-gigs-compr)]])
+   [:div.gigs (get-gigs)]])
