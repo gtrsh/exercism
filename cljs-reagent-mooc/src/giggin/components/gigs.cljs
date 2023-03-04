ec6783aeb6778cab9df362e6101be477a84383f8
Gregory Tereshko <gtrsh@users.noreply.github.com>
2023-03-04 20:54:07 +0300

    [cljs]: use let binding for ev handlers

--- cljs-reagent-mooc/src/giggin/components/gigs.cljs
+++ cljs-reagent-mooc/src/giggin/components/gigs.cljs
@@ -2,21 +2,18 @@
   (:require [giggin.state :as state]
             [giggin.helpers :refer [format-price]]))
 
-(defn get-gigs
-  []
-  (for [{:keys [id img title price desc]} (vals @state/gigs)]
-    [:div.gig {:key id}
-     [:img.gig__artwork {:src img :alt title}]
-     [:div.gig__body
-      [:div.gig__title
-       [:div.btn.btn--primary.float--right.tooltip
-        {:data-tooltip "Add to order"
-         :on-click #(swap! state/orders update id inc)}
-        [:i.icon.icon--plus]] title]
-      [:p.gig__price (format-price price)]
-      [:p.gig__desc desc]]]))
-
 (defn gigs
   []
-  [:main
-   [:div.gigs (get-gigs)]])
+  (let [add-order #(swap! state/orders update % inc)]
+    [:main
+     [:div.gigs (for [{:keys [id img title price desc]} (vals @state/gigs)]
+                  [:div.gig {:key id}
+                   [:img.gig__artwork {:src img :alt title}]
+                   [:div.gig__body
+                    [:div.gig__title
+                     [:div.btn.btn--primary.float--right.tooltip
+                      {:data-tooltip "Add to order"
+                       :on-click #(add-order id)}
+                      [:i.icon.icon--plus]] title]
+                    [:p.gig__price (format-price price)]
+                    [:p.gig__desc desc]]])]]))
--- cljs-reagent-mooc/src/giggin/components/orders.cljs
+++ cljs-reagent-mooc/src/giggin/components/orders.cljs
@@ -10,33 +10,35 @@
 
 (defn orders
   []
-  [:aside
-   (if (empty? @state/orders)
-     [:div.empty
-      [:div.title "Tou don't have any orders"]
-      [:div.subtitle "Click on a + to add an order"]]
-     [:div.order
-      [:div.body
-       (for [[id quant] @state/orders]
-         [:div.item {:key id}
-          [:div.img
-           [:img {:src (get-in @state/gigs [id :img])
-                  :alt (get-in @state/gigs [id :title])}]]
-          [:div.content
-           [:p.title (str (get-in @state/gigs [id :title]) " \u00D7 " quant)]]
+  (let [remove-order #(swap! state/orders dissoc %)
+        remove-all-orders #(reset! state/orders {})]
+    [:aside
+     (if (empty? @state/orders)
+       [:div.empty
+        [:div.title "Tou don't have any orders"]
+        [:div.subtitle "Click on a + to add an order"]]
+       [:div.order
+        [:div.body
+         (for [[id quant] @state/orders]
+           [:div.item {:key id}
+            [:div.img
+             [:img {:src (get-in @state/gigs [id :img])
+                    :alt (get-in @state/gigs [id :title])}]]
+            [:div.content
+             [:p.title (str (get-in @state/gigs [id :title]) " \u00D7 " quant)]]
+            [:div.action
+             [:div.price (format-price (* (get-in @state/gigs [id :price]) quant))]
+             [:button.btn.btn--link.tooltip
+              {:data-tooltip "Remove"
+               :on-click #(remove-order id)}
+              [:i.icon.icon--cross]]]])]
+        [:div.total
+         [:hr]
+         [:div.item
+          [:div.content "Total"]
           [:div.action
-           [:div.price (format-price (* (get-in @state/gigs [id :price]) quant))]
-           [:button.btn.btn--link.tooltip
-            {:data-tooltip "Remove"
-             :on-click #(swap! state/orders dissoc id)}
-            [:i.icon.icon--cross]]]])]
-      [:div.total
-       [:hr]
-       [:div.item
-        [:div.content "Total"]
-        [:div.action
-         [:div.price (format-price (total))]]
-        [:button.btn.btn--link.tooltip
-         {:data-tooltip "Remove all"
-          :on-click #(reset! state/orders {})}
-         [:i.icon.icon--delete]]]]])])
+           [:div.price (format-price (total))]]
+          [:button.btn.btn--link.tooltip
+           {:data-tooltip "Remove all"
+            :on-click #(remove-all-orders)}
+           [:i.icon.icon--delete]]]]])]))
