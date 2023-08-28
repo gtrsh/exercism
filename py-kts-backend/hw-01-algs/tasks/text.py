86f41c9453d7b213e8dcd92eff9a7a8c00cbe256
Gregory Tereshko <gtrsh@users.noreply.github.com>
2023-08-28 14:19:10 +0300

    [py-kts][hw-01] complete text task

--- py-kts-backend/hw-01-algs/tasks/text.py
+++ py-kts-backend/hw-01-algs/tasks/text.py
@@ -10,4 +10,5 @@ def find_shortest_longest_word(text: str) -> tuple[Optional[str], Optional[str]]
     В переданном тексте вернуть слово имеющее наименьшую и наибольшую длину.
     Если такого слова нет - вернуть None
     """
-    raise NotImplementedError
+    words = text.split()
+    return (min(words, key=len), max(words, key=len)) if words else (None, None)
