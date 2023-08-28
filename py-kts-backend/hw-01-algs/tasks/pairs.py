25560986852f512bc1132d5681ae47e945d3b72d
Gregory Tereshko <gtrsh@users.noreply.github.com>
2023-08-28 14:11:08 +0300

    [py-kts][hw-01] complete pairs task

--- py-kts-backend/hw-01-algs/tasks/pairs.py
+++ py-kts-backend/hw-01-algs/tasks/pairs.py
@@ -10,4 +10,4 @@ def corresponding_pairs(arr1: list[Any], arr2: list[Any]) -> list[tuple[Any, Any
     Функция должна возвращать соответствующие элементы двух массивов:
     corresponding_pairs([1, 2], [3, 4]) == [(1, 3), (2, 4)]
     """
-    raise NotImplementedError
+    return list(zip(arr1, arr2))
