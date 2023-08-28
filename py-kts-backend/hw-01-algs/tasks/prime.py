5b106de34d78e16bd870678e0f5bfa846e8a3117
Gregory Tereshko <gtrsh@users.noreply.github.com>
2023-08-28 14:12:23 +0300

    [py-kts][hw-01] complete prime task

--- py-kts-backend/hw-01-algs/tasks/prime.py
+++ py-kts-backend/hw-01-algs/tasks/prime.py
@@ -7,4 +7,14 @@ def is_prime(number: int) -> bool:
     """
     Функция должна вернуть True если число является простым, иначе - False
     """
-    raise NotImplementedError
+    if number == 0 or number == 1:
+        return False
+    else:
+        k = 0
+        for i in range(2, number // 2 + 1):
+            if number % i == 0:
+                k += 1
+        if k == 0:
+            return True
+        else:
+            return False
