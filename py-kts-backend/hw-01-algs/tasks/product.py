6850b8e2e16c33690440d9a095c53921417ee3e1
Gregory Tereshko <gtrsh@users.noreply.github.com>
2023-08-28 14:13:56 +0300

    [py-kts][hw-01] complete product task

--- py-kts-backend/hw-01-algs/tasks/product.py
+++ py-kts-backend/hw-01-algs/tasks/product.py
@@ -1,3 +1,4 @@
+from itertools import product
 from typing import Any
 
 __all__ = (
@@ -10,4 +11,4 @@ def cartesian_product(arr1: list[Any], arr2: list[Any]) -> list:
     Должна возвращать все пары элементы двух массивов:
     cartesian_product([1, 2], [3, 4]) == [(1, 3), (1, 4), (2, 3), (2, 4)]
     """
-    raise NotImplementedError
+    return list(product(arr1, arr2))
