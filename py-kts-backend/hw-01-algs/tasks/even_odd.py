9e78f9648b8a78a8903765cc997776cd02cef01a
Gregory Tereshko <gtrsh@users.noreply.github.com>
2023-08-28 14:04:08 +0300

    [py-kts][hw-01] complete even_odd task

--- py-kts-backend/hw-01-algs/tasks/even_odd.py
+++ py-kts-backend/hw-01-algs/tasks/even_odd.py
@@ -3,10 +3,14 @@ __all__ = (
 )
 
 
-def even_odd(arr: list[int]) -> float:
+def even_odd(xs: list[int]) -> float:
     """
     Функция возвращает отношение суммы четных элементов массив к сумме нечетных
     Пример:
     even_odd([1, 2, 3, 4, 5]) == 0.8889
     """
-    raise NotImplementedError
+    a = sum([i for i in xs if i % 2 == 0])
+    b = sum([i for i in xs if i % 2 != 0])
+    if a == 0 or b == 0:
+        return 0
+    return a / b
