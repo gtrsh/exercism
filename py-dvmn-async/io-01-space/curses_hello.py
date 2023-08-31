e53060dfa7ee0cd6b82f22fcdaab68c2c5c6f822
Gregory Tereshko <gtrsh@users.noreply.github.com>
2023-09-01 00:16:21 +0300

    [py-dvmn][io-01] add border and screen init

--- py-dvmn-async/io-01-space/curses_hello.py
+++ py-dvmn-async/io-01-space/curses_hello.py
@@ -2,10 +2,13 @@ import time
 import curses
 
 def draw(canvas, row, col, delay):
+    canvas.border()
     canvas.addstr(row, col, 'Hello, world!')
     canvas.refresh()
     time.sleep(delay)
 
 if __name__ == '__main__':
+    curses.initscr()
     curses.update_lines_cols()
+    curses.curs_set(False)
     curses.wrapper(lambda cnv: draw(cnv, 10, 20, 1))
