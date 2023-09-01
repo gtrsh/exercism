ad010cc0e959ef0ffc4790f4c3bf3ba5ead1462b
Gregory Tereshko <gtrsh@users.noreply.github.com>
2023-09-02 01:50:57 +0300

    [py-dvmn][io-01] add blinking star

--- py-dvmn-async/io-01-space/curses_hello.py
+++ py-dvmn-async/io-01-space/curses_hello.py
@@ -1,14 +1,29 @@
 import time
 import curses
 
-def draw(canvas, row, col, delay):
+def draw(canvas):
+    row, col = (10, 20)
     canvas.border()
-    canvas.addstr(row, col, 'Hello, world!')
-    canvas.refresh()
-    time.sleep(delay)
+
+    while True:
+        canvas.addstr(row, col, '*', curses.A_DIM)
+        canvas.refresh()
+        time.sleep(2)
+
+        canvas.addstr(row, col, '*')
+        canvas.refresh()
+        time.sleep(0.3)
+
+        canvas.addstr(row, col, '*', curses.A_BOLD)
+        canvas.refresh()
+        time.sleep(0.5)
+
+        canvas.addstr(row, col, '*')
+        canvas.refresh()
+        time.sleep(0.3)
 
 if __name__ == '__main__':
     curses.initscr()
     curses.update_lines_cols()
     curses.curs_set(False)
-    curses.wrapper(lambda cnv: draw(cnv, 10, 20, 1))
+    curses.wrapper(draw)
