b8d29038ec01358919191ef0d407a6b4e20f7da0
Gregory Tereshko <gtrsh@users.noreply.github.com>
2023-09-12 00:10:16 +0300

    [py-bizly]: add f-string formatting

--- py-bizly/interest/logic.py
+++ py-bizly/interest/logic.py
@@ -2,5 +2,5 @@ def interest(principal, rate, numyears):
     year = 1
     while year <= numyears:
         principal = principal * (1 + rate)
-        print(year, principal)
+        print(f'{year:>3d} {principal:0.2f}')
         year += 1
