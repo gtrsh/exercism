e13faf56e59b582b71af9a368a869675a17f081d
Gregory Tereshko <gtrsh@users.noreply.github.com>
2025-07-29 18:27:43 +0300

    complete login route

--- node-fastify-workshop/src/index.js
+++ node-fastify-workshop/src/index.js
@@ -1,5 +1,6 @@
 const createApp = async (app) => {
   app.register(import('./routes/users.js'), { prefix: '/users' })
+  app.register(import('./routes/login.js'), { prefix: '/login' })
 
   return app
 }
---
+++ node-fastify-workshop/src/routes/login.js
@@ -0,0 +1,15 @@
+import S from 'fluent-json-schema'
+
+const schema = {
+  body: S.object()
+    .prop('username', S.string()).required()
+    .prop('password', S.string()).required()
+}
+
+export default async (fastify, opts) => {
+  fastify.post('/', { schema },  async (req, reply) => {
+    const { username } = req.body
+
+    return { username }
+  })
+}
