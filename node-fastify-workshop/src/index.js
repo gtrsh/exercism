1fd6441875ddd05c3da446678c6dcf6a767f7256
Gregory Tereshko <gtrsh@users.noreply.github.com>
2025-07-29 18:41:49 +0300

    remove version route

--- node-fastify-workshop/src/index.js
+++ node-fastify-workshop/src/index.js
@@ -1,7 +1,6 @@
 const createApp = async (app) => {
   app.register(import('./routes/users.js'), { prefix: '/users' })
   app.register(import('./routes/login.js'), { prefix: '/login' })
-  app.register(import('./routes/version.js'))
 
   return app
 }
