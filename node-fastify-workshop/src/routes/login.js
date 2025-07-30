6a9076ecd06fd8909c26522523fb6d83798ba821
Gregory Tereshko <gtrsh@users.noreply.github.com>
2025-07-30 14:08:48 +0300

    complete response validation

--- node-fastify-workshop/src/routes/login.js
+++ node-fastify-workshop/src/routes/login.js
@@ -3,7 +3,7 @@ import S from 'fluent-json-schema'
 const schema = {
   body: S.object()
     .prop('username', S.string()).required()
-    .prop('password', S.string()).required()
+    .prop('password', S.string()).required(),
 }
 
 export default async (fastify, opts) => {
--- node-fastify-workshop/src/routes/users.js
+++ node-fastify-workshop/src/routes/users.js
@@ -1,10 +1,21 @@
+import S from 'fluent-json-schema'
+
+const schema = {
+  response: {
+    200: S.array().items(
+      S.object().prop('username', S.string()).required()
+    )
+  }
+}
+
 export default async (fastify, opts) => {
-  fastify.get('/', async (req, reply) => {
+  fastify.get('/', { schema }, async (req, reply) => {
     req.log.info('get users request')
 
     return [
-      {username: 'john-doe'},
-      {username: 'eva-lu-ator'},
+      { username: 'john-doe' },
+      { username: 'eva-lu-ator' },
+      // { otherfld: 'eva-lu-ator' },
     ]
   })
 }
